cfa47c974bb95449ff5f58a637aa0aad4fe9cd70
emmanuel.ngwandu <emmanuel.ngwandu@lands.go.tz>
2024-08-20 17:25:05 +0300

    users table

--- lib/neon.server.ts
+++ lib/neon.server.ts
@@ -1,3 +1,11 @@
 import {neon} from '@neondatabase/serverless'
 
 export const sql = neon("postgresql://fitness_owner:o9EiBLCeZ4dS@ep-fancy-bread-a5haf8f6.us-east-2.aws.neon.tech/fitness?sslmode=require") 
+
+/*
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password TEXT NOT NULL
+);
+*/
