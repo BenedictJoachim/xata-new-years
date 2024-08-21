c3c28e88efe0f878a1a3f6baabd659fafaf36faf
BenedictJoachim <148780517+BenedictJoachim@users.noreply.github.com>
2024-08-21 12:25:17 +0000

    Checkbox functionality initial implementation

--- app/components/Resolution.tsx
+++ app/components/Resolution.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { Form, useSubmit } from '@remix-run/react';
+import React, { ChangeEventHandler } from 'react';
 import type { ResolutionProp } from "~/routes/resolutions";
 
 interface ResolutionListProps {
@@ -6,19 +7,32 @@ interface ResolutionListProps {
 }
 
 const Resolution = ({ resolution }: ResolutionListProps) => {
+    const submit = useSubmit()
+
+    const handleChange: ChangeEventHandler<HTMLFormElement> = event => {
+        submit(event.currentTarget)
+    }
+
     return (
         <div className="flex flex-row items-center justify-between p-4 bg-gray-800 rounded-lg shadow-md">
             <div className="flex justify-center items-center space-x-2">
                 <p className="text-gray-500">{resolution.year}</p>
-                <h2 className="text-lg text-gray-500 font-semibold">{resolution.resolution}</h2>
+                <h2 className={`text-lg text-gray-500 font-semibold ${resolution.isComplete ? "line-through" : "no-underline"}`}>
+                    {resolution.resolution}
+                </h2>
             </div>
             <div className="flex items-center">
-                <input
-                    type="checkbox"
-                    checked={resolution.isComplete}
-                    readOnly
-                    className="mr-2"
-                />
+                <Form method='post' onChange={handleChange}>
+                    <input
+                        type="checkbox"
+                        name='isComplete'
+                        checked={resolution.isComplete}
+                        readOnly
+                        className="mr-2"
+                    />
+                    <input type="hidden" name="id" value={resolution.id} />
+                    <input type="hidden" name="action" value="complete" />
+                </Form>
                 <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
                     Edit
                 </button>
